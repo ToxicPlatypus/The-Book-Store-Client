d0469ff8107962325f4319f8565be33e3e815b19
ToxicPlatypus <rafidrabbi123@gmail.com>
2022-05-18 16:03:12 +0600

    json

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Service.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Service.css";
 
-const Service = ({service}) => {
-    const {id, name, img, description, price} = service;
-    const navigate = useNavigate();
+const Service = ({ service }) => {
+  const { id, name, img, description, price, supplier, quantity } = service;
+  const navigate = useNavigate();
 
-    const navigateToServiceDetail = id =>{
-        navigate(`/service/${id}`);
-    }
-    return (
-        <div className='service'>
-            <img className='w-100' src={img} alt="" />
-            <h2>{name}</h2>
-            <p>Price: {price}</p>
-            <p><small>{description}</small></p>
-            <button onClick={() => navigateToServiceDetail(id)} className='btn btn-primary'>Book: {name}</button>
-        </div>
-    );
+  const navigateToServiceDetail = (id) => {
+    navigate(`/service/${id}`);
+  };
+  return (
+    <div className="service">
+      <img className="w-100" src={img} alt="" />
+      <h2>{name}</h2>
+      <p>Price: {price}</p>
+      <p>
+        <small>{description}</small>
+      </p>
+      <p className="fw-bold">Supplier: {supplier}</p>
+      <h5>Available: {quantity} sets</h5>
+
+      <button
+        onClick={() => navigateToServiceDetail(id)}
+        className="btn btn-primary"
+      >
+        Book: {name}
+      </button>
+    </div>
+  );
 };
 
 export default Service;
