733664a838a38c68eee8e7dcb04011012d1cff45
ToxicPlatypus <rafidrabbi123@gmail.com>
2022-05-19 12:13:25 +0600

    comment box

--- src/Pages/Comment/Comment.js
+++ src/Pages/Comment/Comment.js
@@ -1,9 +1,28 @@
 import React from "react";
 
 const Comment = () => {
+  function refreshPage() {
+    window.location.reload(false);
+  }
   return (
     <div className="container">
-      <h2>Please Leave a Comment</h2>
+      <h2 className="text-center text-primary">Please Leave a Comment</h2>
+      <div class="form-group purple-border w-50 mx-auto mt-4">
+        <label for="exampleFormControlTextarea4">
+          Enter your Comment Below
+        </label>
+        <textarea
+          class="form-control"
+          id="exampleFormControlTextarea4"
+          rows="3"
+        ></textarea>
+        <button
+          onClick={refreshPage}
+          className="btn btn-primary text-center mt-2 mb-4"
+        >
+          Comment
+        </button>
+      </div>
     </div>
   );
 };
